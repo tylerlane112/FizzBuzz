daabbecd38d23d20d38ff4b927dab6319acb10b3
Tyler Lane <tylerlane112@hotmail.com>
2024-09-18 21:27:13 +0100

    Create testing project, add DividerAttribute

---
+++ HealthPartnersTechnicalTest.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace HealthPartnersTechnicalTest.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ HealthPartnersTechnicalTest/Attributes/DividerAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthPartnersTechnicalTest.Attributes
+{
+    public class DividerAttribute : Attribute
+    {
+        public int Divider;
+
+        public DividerAttribute(int divider)
+        {
+            this.Divider = divider;
+        }
+    }
+}
