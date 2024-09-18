a25742940e888d483e4163a26c3a0937090679f7
Tyler Lane <tylerlane112@hotmail.com>
2024-09-18 22:08:14 +0100

    Add FizzBuzzRunner skeleton method and test file

---
+++ HealthPartnersTechnicalTest/FizzBuzzRunner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthPartnersTechnicalTest
+{
+    internal class FizzBuzzRunner
+    {
+        public List<string> ExecuteFizzBuzz()
+        {
+            return new List<string>();
+        }
+
+    }
+}
